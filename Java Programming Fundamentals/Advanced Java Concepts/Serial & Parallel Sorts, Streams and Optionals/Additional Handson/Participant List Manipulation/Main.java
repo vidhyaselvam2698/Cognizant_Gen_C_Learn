699ef87785bc320e5f6d5cc02c0efcbadf909463
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-11-09 17:50:21 +0530

    Refact for Main class in Participant List Manipulation

--- Java Programming Fundamentals/Advanced Java Concepts/Serial & Parallel Sorts, Streams and Optionals/Additional Handson/Participant List Manipulation/Main.java
+++ Java Programming Fundamentals/Advanced Java Concepts/Serial & Parallel Sorts, Streams and Optionals/Additional Handson/Participant List Manipulation/Main.java
@@ -3,50 +3,49 @@ import java.util.stream.Stream;
 
 
 public class Main {
-
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        ParticipantUtility participantUtility = new ParticipantUtility();
         int option = 0;
-        String participantName;
-        String departmentName;
-        String collegeName;
-        ParticipantUtility viewObj = new ParticipantUtility();
 
         do {
             System.out.println("1.Add\n2.View\n3.Exit");
-            option = sc.nextInt();
-            sc.nextLine();
+            option = scanner.nextInt();
+            scanner.nextLine();
             switch (option) {
-                case 1:
-                    participantName = sc.nextLine();
-                    departmentName = sc.nextLine();
-                    collegeName = sc.nextLine();
-                    Participant pObj = new Participant();
-                    pObj.setParticipantName(participantName);
-                    pObj.setDepartmentName(departmentName);
-                    pObj.setCollegeName(collegeName);
-                    viewObj.addParticipant(pObj);
-                    break;
-                case 2:
-                    System.out.println("Enter department name");
-                    departmentName = sc.nextLine();
-                    System.out.println("Enter college name");
-                    collegeName = sc.nextLine();
+                case 1: {
+                    String participantName = scanner.nextLine();
+                    String departmentName = scanner.nextLine();
+                    String collegeName = scanner.nextLine();
 
-                    Stream<Participant> filteredParticipantStream = viewObj.viewAllParticipants(departmentName, collegeName);
+                    Participant participant = new Participant();
+                    participant.setParticipantName(participantName);
+                    participant.setDepartmentName(departmentName);
+                    participant.setCollegeName(collegeName);
+                    participantUtility.addParticipant(participant);
+
+                    break;
+                }
+                case 2: {
+                    System.out.println("Enter department name");
+                    String departmentName = scanner.nextLine();
+                    System.out.println("Enter college name");
+                    String collegeName = scanner.nextLine();
+
+                    Stream<Participant> filteredParticipantStream = participantUtility.viewAllParticipants(departmentName, collegeName);
                     filteredParticipantStream.forEach(System.out::println);
 
                     break;
-                case 3:
+                }
+                case 3: {
                     System.out.println("Thank you for using the application");
                     break;
-                default:
+                }
+                default: {
                     System.out.println("Please choose a correct option");
                     break;
+                }
             }
         } while (option != 3);
-
     }
-
 }
