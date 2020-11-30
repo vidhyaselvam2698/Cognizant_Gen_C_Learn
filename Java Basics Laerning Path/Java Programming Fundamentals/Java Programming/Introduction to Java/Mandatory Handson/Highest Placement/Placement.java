d14b91ad057550b1d0e3088d587efc410dbcd5d4
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-11-30 20:36:53 +0530

    Update Highest Placement

--- Java Basics Laerning Path/Java Programming Fundamentals/Java Programming/Introduction to Java/Mandatory Handson/Highest Placement/Placement.java
+++ Java Basics Laerning Path/Java Programming Fundamentals/Java Programming/Introduction to Java/Mandatory Handson/Highest Placement/Placement.java
@@ -1,55 +1,65 @@
 import java.util.*;
-import java.util.stream.*;
+
+public class Placement {
+    private static class Department implements Comparable<Department> {
+        private final String name;
+        private final int placements;
+
+        public Department(String name, int placements) {
+            this.name = name;
+            this.placements = placements;
+        }
+
+        public int getPlacements() {
+            return placements;
+        }
 
 
-class Placement {
+        @Override
+        public int compareTo(Department other) {
+            return Comparator.comparingInt(Department::getPlacements)
+                    .compare(this, other);
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int cse, ece, mech;
-        
+
         System.out.println("Enter the no of students placed in CSE:");
-        cse = scanner.nextInt();
+        int cs = scanner.nextInt();
         System.out.println("Enter the no of students placed in ECE:");
-        ece = scanner.nextInt();
-        System.out.println("Enter the no of students placed in MECH");
-        mech = scanner.nextInt();
-        
-        List<Integer> list = new TreeSet<>(Arrays.asList(cse, ece, mech)).stream().collect(Collectors.toList());
-        
-        if (list.get(0) < 0) {
+        int ec = scanner.nextInt();
+        System.out.println("Enter the no of students placed in MECH:");
+        int me = scanner.nextInt();
+
+        if (cs < 0 || ec < 0 || me < 0) {
             System.out.println("Input is Invalid");
-        } else if (list.size() == 1) {
-            System.out.println("None of the department has got the highest placement");
         } else {
-            System.out.println("Highest placement");
-            
-            int highest = list.get(list.size() - 1);
-            
-            if (list.size() == 2) {
-                if (cse == highest && ece == highest) {
-                    System.out.println("CSE");
-                    System.out.println("ECE");
-                } else if (cse == highest && mech == highest) {
-                    System.out.println("CSE");
-                    System.out.println("MECH");
-                } else if (ece == highest && mech == highest) {
-                    System.out.println("ECE");
-                    System.out.println("MECH");
-                } else if (cse == highest) {
-                    System.out.println("CSE");
-                } else if (ece == highest) {
-                    System.out.println("ECE");
-                } else if (mech == highest) {
-                    System.out.println("MECH");
+            Department cse = new Department("CSE", cs);
+            Department ece = new Department("ECE", ec);
+            Department mech = new Department("MECH", me);
+
+            ArrayList<Department> departmentArrayList = new ArrayList<>(Arrays.asList(cse, ece, mech));
+            TreeSet<Department> departmentSet = new TreeSet<Department>(Comparator.reverseOrder()) {
+                {
+                    add(cse);
+                    add(ece);
+                    add(mech);
                 }
+            };
+
+            if (departmentSet.size() == 1) {
+                System.out.println("None of the department has got the highest placement");
             } else {
-                if (cse == highest) {
-                    System.out.println("CSE");
-                } else if (ece == highest) {
-                    System.out.println("ECE");
-                } else if (mech == highest) {
-                    System.out.println("MECH");
-                }
+                System.out.println("Highest placement");
+                departmentArrayList.stream()
+                        .filter(department -> department.placements == departmentSet.first().placements)
+                        .forEach(System.out::println);
             }
         }
     }
